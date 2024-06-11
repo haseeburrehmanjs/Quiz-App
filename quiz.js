1ac327194fcf9672acfa7b55b11869a41e1b4b7b
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-06-11 15:56:10 +0500

    80% work is done

--- quiz.js
+++ quiz.js
@@ -1,33 +1,75 @@
 // GET HTML ELEMENT IN JAVASCRIPT
 let div = document.querySelector('.question')
 let nextBtn = document.querySelector('#nextBtn')
+let ul = document.querySelector('.main-ul')
 let index = 0
-let question = []
+let questions = [] // Initialize an empty array to store the questions
 
-let renderArray = array => {
-    if (index < question.length) {
-        let questionArray = array.data
-        console.log(questionArray);
-        div.innerHTML = `
-        <h1>${questionArray[index].question.text}</h1>
-        `
-    } else {
-        console.log('Question Complete');
+// SUFFLE ARRAY 
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
     }
-
+    return arr;
 }
 
+
+// RENDER ITEM ON QUIZ.HTML
+let renderQuestion = (arr) => {
+    div.innerHTML = ''
+    let answer = [
+        ...arr[index].incorrectAnswers, arr[index].correctAnswer
+    ]
+    console.log(answer);
+    div.innerHTML += `
+      <h3>Q${index + 1}: ${arr[index].question.text}</h3>
+      `;
+      ul.innerHTML = `
+      ${shuffleArray(answer).map(
+        (items) => `
+        <li>
+        <input type="radio" name="choice" class="choice" id=${items} value=${items}><label for=${items}> ${items}</label>
+        </li>`
+      )}
+      `
+}
+
+// NEXT BUTTON
+nextBtn.addEventListener("click", () => {
+    if (index < questions.length - 1) {
+        index += 1
+        renderQuestion(questions)
+    } else {
+        alert('Go to Result')
+        window.location= 'result.html'
+    }
+})
+
+// API CALL DATA 
 axios("https://the-trivia-api.com/v2/questions")
-    .then(responce => {
-        renderArray(responce)
-        console.log(responce);
+    .then(response => {
+        questions = response.data // Store the fetched questions in the questions array
+        renderQuestion(questions) // Render the first question
+        console.log(response);
     })
     .catch(error => {
         console.log(error);
     })
 
-// NEXT BUTTON
-nextBtn.addEventListener('click', () => {
-    index += 1
-    renderArray(question)
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
